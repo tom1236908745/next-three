7bd0d3450b9ee0ee6f76a1e2a984dbfe3fa9460e
tom1236908745 <tom1233908745@gmail.com>
2021-09-08 20:37:48 +0900

    fix for bug

--- pages/sample4.jsx
+++ pages/sample4.jsx
@@ -17,6 +17,62 @@ extend({ LUTPass });
 function Grading() {
   const { texture3D } = useLoader(LUTCubeLoader, "images/cubicle-99.CUBE");
   return (
+    <Effects>children={<lUTPass attachArray="passes" lut={texture3D} />}</Effects>
+  );
+}
+
+function Sphere(props) {
+  const texture = useTexture("images/terrazo.png");
+  return (
+    <mesh {...props}>
+      <sphereBufferGeometry args={[0.5, 64, 64]} />
+      <meshPhysicalMaterial
+        envMapIntensity={0.4}
+        map={texture}
+        clearcoat={0.8}
+        clearcoatRoughness={0}
+        roughness={1}
+        metalness={0.9}
+      />
+    </mesh>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <h3 className={styles.character}> react - three - fieber </h3>{" "}
+      <Canvas
+        className={styles.root}
+        frameloop="demand"
+        dpr={[1, 2]}
+        camera={{ position: [0, 0, 5], fov: 20 }}
+      >
+        <spotLight
+          intensity={0.5}
+          angle={0.2}
+          penumbra={1}
+          position={[5, 15, 10]}
+        />
+        <pointLight position={[-20, -5, -10]} color="red" intensity={0.8} />
+        <Suspense fallback={null}>
+          <Sphere />
+          <Grading />
+          <Environment preset="warehouse" />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+      <Loader />
+    </>
+  );
+}
+
+/* 
+extend({ LUTPass });
+
+function Grading() {
+  const { texture3D } = useLoader(LUTCubeLoader, "images/cubicle-99.CUBE");
+  return (
     <Effects><lUTPass attachArray="passes" lut={texture3D} /></Effects>
   );
 }
@@ -70,3 +126,4 @@ export default function Sample4() {
     </>
   );
 }
+ */
